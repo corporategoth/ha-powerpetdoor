8076bfc84cda320c65a5dc853efedd5a1f7b2f80
Preston A. Elder <prez@neuromancy.net>
2022-03-02 06:56:09 -0500

    Fixed error in config_flow.py

--- custom_components/powerpetdoor/config_flow.py
+++ custom_components/powerpetdoor/config_flow.py
@@ -1,4 +1,4 @@
-"""Adds config flow for dnsip integration."""
+"""Adds config flow for power pet door integration."""
 from __future__ import annotations
 
 import asyncio
@@ -17,7 +17,7 @@ from homeassistant.const import (
     CONF_HOST,
     CONF_PORT,
     CONF_TIMEOUT,
-}
+)
 
 from .const import (
     DOMAIN,
@@ -48,10 +48,10 @@ DATA_SCHEMA_ADV = vol.Schema({
     vol.Optional(CONF_RECONNECT, default=DEFAULT_RECONNECT_TIMEOUT): cv.time_period_seconds,
     vol.Optional(CONF_KEEP_ALIVE, default=DEFAULT_KEEP_ALIVE_TIMEOUT): cv.time_period_seconds,
     vol.Optional(CONF_REFRESH, default=DEFAULT_REFRESH_TIMEOUT): cv.time_period_seconds,
-)
+    })
 
 class PowerPetDoorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for dnsip integration."""
+    """Handle a config flow for power pet door integration."""
 
     VERSION = 1
 
@@ -68,6 +68,7 @@ class PowerPetDoorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle the initial step."""
 
+        errors = {}
         if user_input is not None:
             host = user_input.get(CONF_HOST)
             name = user_input.get(CONF_NAME)
@@ -78,6 +79,27 @@ class PowerPetDoorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=name, data=user_input)
 
         if self.show_advanced_options is True:
-            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA_ADV)
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA_ADV, errors)
 
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors)
+
+class PowerPetDoorOptionsFlow(config_entries.OptionsFlow):
+    """Handle a option config for power pet door integration."""
+
+    def __init__(self, entry: config_entries.ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.entry = entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title=name, data=user_input)
+
+        if self.show_advanced_options is True:
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA_ADV, errors)
+
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors)
