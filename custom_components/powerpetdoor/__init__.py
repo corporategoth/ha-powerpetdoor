63d28b34de9cfaa7c7e162b7e08884befe427876
Preston A. Elder <prez@neuromancy.net>
2022-03-02 01:20:56 -0500

    Trying adding to init

--- custom_components/powerpetdoor/__init__.py
+++ custom_components/powerpetdoor/__init__.py
@@ -1,1 +1,8 @@
 """ Power Pet Door """
+
+from homeassistant.const import Platform
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.config_entries.async_setup_platforms(entry, [ Platform.SWITCH ])
+
+    return True
