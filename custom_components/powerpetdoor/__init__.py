0f518c72255a032bc1c7cb50adcdb4c1fc2988de
Preston A. Elder <prez@neuromancy.net>
2022-03-02 01:26:30 -0500

    Clear out init.py

--- custom_components/powerpetdoor/__init__.py
+++ custom_components/powerpetdoor/__init__.py
@@ -1,8 +1,1 @@
 """ Power Pet Door """
-
-from homeassistant.const import Platform
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.config_entries.async_setup_platforms(entry, [ Platform.SWITCH ])
-
-    return True
